5ceee70e7930d3ddce68dc53140acb7a0f3cb01e
Zhi <ALPHONSEWAN@GMAIL.COM>
2024-08-26 21:42:53 +0800

    Update script.js

--- css/script.js
+++ css/script.js
@@ -5,6 +5,11 @@ let navlist = document.querySelector('.navlist')
 menu.onclick = () => {
   menu.classList.toggle('bx-x');
   navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navlist.classList.remove('open')
 }
 
 
