efc5954f8b34cc73d6e817c482d8d04b9a8f3035
Zhi <ALPHONSEWAN@GMAIL.COM>
2024-08-29 18:04:05 +0800

    update

--- css/script.js
+++ css/script.js
@@ -40,4 +40,64 @@ window.addEventListener("scroll", function(){
   }
 )
 
+//Typing Effect//
+/*
+ * typingEffect()
+ * It types an array of texts in a random order. I like random stuff🙃
+ */
+function typingEffect() {
+  const contactTexts = shuffleArray(["一个对动效设计充满学习热情的UI/UX设计师", "A UI/UX designer with experience and a passion for exploring designs and motion effects."]);
+  const herop = document.getElementsByClassName("hero-p")[0];
+  let removing = false;
+  let idx = char = 0;
+
+  setInterval(() => { // We define the interval of the typing speed
+
+      // If we do not reach the limit, we insert characters in the html
+      if (char < contactTexts[idx].length) herop.innerHTML += contactTexts[idx][char];
+
+      // 15*150ms = time before starting to remove characters
+      if (char == contactTexts[idx].length + 50) removing = true;
+
+      // Removing characters, the last one always
+      if (removing) herop.innerHTML = herop.innerHTML.substring(0, herop.innerHTML.length - 1);
+
+      char++; // Next character
+
+      // When there is nothing else to remove
+      if (herop.innerHTML.length === 0) {
+
+          // If we get to the end of the texts we start over
+          if (idx === contactTexts.length - 1) idx = 0
+          else idx++;
+
+          char = 0; // Start the next text by the first character
+          removing = false; // No more removing characters
+      }
+
+  }, 80); // Typing speed, 150 ms
+
+}
+typingEffect();
+function shuffleArray(array) {
+  let currentIndex = array.length,
+      temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
 
