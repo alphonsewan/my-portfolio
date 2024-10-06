4cb0d8db51ba915e2bc0fc4a60bf86a94ae1f69b
Zhi <ALPHONSEWAN@GMAIL.COM>
2024-10-06 23:29:53 +0800

    update

--- proj/css/proj.js
+++ proj/css/proj.js
@@ -18,26 +18,12 @@ const handleToggle = () => {
 toggle.addEventListener("click", handleToggle);
 // ***End Light/Dark Mode Switch Button 明暗模式切换//
 
-//Language Switch Button 语言切换//
-document.querySelector(".language").addEventListener("click", function () {
-  const currentLanguage = document.documentElement.lang;
 
-  // 根据当前语言重定向到相应的页面
-  if (currentLanguage === "en") {
-    window.location.href = "komet.html"; // 重定向到中文版
-  } else {
-    window.location.href = "komet.html"; // 重定向到英文版
-  }
-});
-// ***End Language Switch Button 语言切换//
-
-//明暗模式以及语言切换按钮的渐入渐出//
+//明暗模式按钮的渐入渐出//
 let lastScrollTop = 0;
-const languageButton = document.querySelector(".language");
 const themeButton = document.querySelector(".theme");
 
 // 将按钮设置为可见
-languageButton.classList.remove("hidden");
 themeButton.classList.remove("hidden");
 
 window.addEventListener("scroll", function () {
@@ -45,17 +31,15 @@ window.addEventListener("scroll", function () {
 
   if (scrollTop > lastScrollTop) {
     // 向下滚动，隐藏按钮
-    languageButton.classList.add("hidden"); // 添加隐藏类
     themeButton.classList.add("hidden"); // 添加隐藏类
   } else {
     // 向上滚动，显示按钮
-    languageButton.classList.remove("hidden"); // 移除隐藏类
     themeButton.classList.remove("hidden"); // 移除隐藏类
   }
 
   lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // 对于移动设备或负滚动
 });
-// ***End 明暗模式以及语言切换按钮的渐入渐出//
+// ***End 明暗模式按钮的渐入渐出//
 
 
 //Scroller 滚轮//
@@ -86,13 +70,3 @@ document.addEventListener("touchmove", updateScrollerRotation);
 // ***End Scroller 滚轮//
 
 
-
-
-// 滚动动画触发
-const REPLAY = () => {
-  const MARKUP = document.body.innerHTML
-  document.body.innerHTML = ""
-  requestAnimationFrame(() => {
-    document.body.innerHTML = MARKUP
-  })
-}
