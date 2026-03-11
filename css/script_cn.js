d93eb6ae2a2cf85d337b0f28e5e7b1f71fd1c232
Zhi <ALPHONSEWAN@GMAIL.COM>
2026-03-12 01:20:26 +0800

    update

--- css/script_cn.js
+++ css/script_cn.js
@@ -40,10 +40,10 @@ document.querySelector(".language").addEventListener("click", function () {
   const currentLanguage = document.documentElement.lang;
 
   // 根据当前语言重定向到相应的页面
-  if (currentLanguage === "en") {
-    window.location.href = "index.html"; // 重定向到中文版
+  if (currentLanguage === "cn") {
+    window.location.href = "/_"; // 重定向到中文版
   } else {
-    window.location.href = "index_en.html"; // 重定向到英文版
+    window.location.href = "/"; // 重定向到英文版
   }
 });
 
